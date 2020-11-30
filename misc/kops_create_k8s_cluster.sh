c8696452a60938f475e5820d3a65e354e753f94a
markcurtis1970 <markcurtis1970@gmail.com>
2020-11-30 17:28:06 +0000

    Updated kops script

--- misc/kops_create_k8s_cluster.sh
+++ misc/kops_create_k8s_cluster.sh
@@ -31,13 +31,13 @@ function create_bucket {
 # Create cluster and validate
 function create_cluster {
     if [ ${DRY_RUN} = "false" ]; then
-        kops create cluster --ssh-public-key=${SSH_KEY} --zones ${REGION} --node-count=2 --node-size=t2.micro --master-size=t2.micro ${NAME}
+        kops create cluster --ssh-public-key=${SSH_KEY} --zones ${REGION} --node-count=$NODE_COUNT --node-size=$NODE_SIZE --master-size=$NODE_SIZE ${NAME} --cloud-labels=$TAGS
         kops edit ig --name=${NAME} master-${REGION}
         kops update cluster ${NAME} --yes
         kops validate cluster --name=${NAME} --wait 10m
     else
         echo "Dry run output - these commands wouuld normally be run"
-        echo kops create cluster --ssh-public-key=${SSH_KEY} --zones ${REGION} --node-count=2 --node-size=t2.micro --master-size=t2.micro ${NAME}
+        echo kops create cluster --ssh-public-key=${SSH_KEY} --zones ${REGION} --node-count=$NODE_COUNT --node-size=$NODE_SIZE --master-size=$NODE_SIZE ${NAME} --cloud-labels=$TAGS
         echo kops edit ig --name=${NAME} master-${REGION}
         echo kops update cluster ${NAME} --yes
         echo kops validate cluster --name=${NAME} --wait 10m
@@ -56,7 +56,10 @@ KOPS_STATE_STORE=""
 AWS_ACCESS_KEY_ID=""
 AWS_SECRET_ACCESS_KEY=""
 REGION="eu-west-2a"
-DRY_RUN=true
+NODE_COUNT=3
+NODE_SIZE="t2.medium"
+TAGS="owner=bill.stickers,department=tech-support"
+DRY_RUN=false
 
 # Run
 creds
